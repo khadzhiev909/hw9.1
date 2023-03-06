02f235447752ee9394aa0a53133fae8a97dcf6b2
Эпенди <khadzhiev909@gmail.com>
2023-03-06 15:08:53 +0300

    Готово.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    private static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета " + fullName.toUpperCase());
+    }
+    private static void task3() {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        fullName = fullName.toUpperCase();
+        fullName = fullName.replace("Ё", "Е");
+
+        System.out.println("Данные ФИО сотрудника " + fullName);
     }
 }
